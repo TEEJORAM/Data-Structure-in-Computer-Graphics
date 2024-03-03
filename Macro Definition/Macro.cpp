90e2961117b13d8fd9cbe036f1f3f6740c3c9468
SWAGAT TEEJORAM <swagat7505@gmail.com>
2024-03-03 13:06:16 +0530

    Macro Definition Updated

---
+++ Macro Definition/Macro.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Macro.h"
+
+int main() {
+    int x = 5, y = 10;
+    std::cout<<"Max of "<<x<<" and "<<y<<" is "<< MAX(x, y)<<std::endl;
+    return 0;
+}
---
+++ Macro Definition/Macro.h
@@ -0,0 +1,7 @@
+#ifndef MACRO_H
+#define MACRO_H
+
+//Macro Definition for finding the maximum of two numbers
+#define MAX(A,B) (((A) > (B) ? (A) : (B)))
+
+#endif
