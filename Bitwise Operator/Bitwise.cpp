19acaa590f6b3e676dcdb4463bd763df21100743
SWAGAT TEEJORAM <swagat7505@gmail.com>
2024-03-03 19:57:09 +0530

    Updated Bitwise Operator

--- Bitwise Operator/Bitwise.cpp
+++ Bitwise Operator/Bitwise.cpp
@@ -3,9 +3,28 @@
 
 void Bitwise::PerformOperation(int A, int B)
 {
+  /*
+  BITWISE AND: (A & B)
+  1. Each bit of the result is 1 only if corresponding bits of both A and B are 1, otherwise it's 0
+  */
   std::cout<<"Bitwise AND: "<<(A & B) <<std::endl;
+
+  /*
+  BITWISE OR: (A | B)
+  1.Each bit of the result is 1 if either of the corresponding bits of A and B is 1, otherwise it's 0
+  */
   std::cout<<"Bitwise OR: "<<(A | B) <<std::endl;
+
+  /*
+  BITWISE XOR: (A ^ B)
+  1.Each bit of the result is 1 if either of the corresponding bits of A and B are different, otherwise it's 0
+  */
   std::cout<<"Bitwise XOR: "<<(A ^ B) <<std::endl;
+
+  /*
+  BITWISE NOT: (~A)
+  1.It flips each bit of A, turning 1s into 0s and 0s into 1s
+  */
   std::cout<<"Bitwise NOT of A: "<<(~A)<<std::endl;
 }
 
